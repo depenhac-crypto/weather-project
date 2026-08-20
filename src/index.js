a0d56326279c8f9c3f232c031b2a392591f79908
depenhac-crypto <depenhac@gmail.com>
2026-08-20 11:07:42 +1000

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "3eabc2tbo0b9bd341497eabfb905d628";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let seachInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = seachInput.value.trim();
+
+  searchCity(seachInput.value.trim());
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Melbourne");
